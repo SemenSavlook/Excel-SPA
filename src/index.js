be6ba7167935b7ef7dee04fc74f3a87d43c426dd
SemenSavlook <semensavluk@gmail.com>
2022-05-12 18:10:20 +0600

    finish table logic, chapter-5

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import { Header } from './components/header/Header';
 import { Table } from './components/table/Table';
 import { Toolbar } from './components/toolbar/Toolbar';
 import './scss/index.scss';
+
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
 });
 
 excel.render();
 
-console.log('Excel: ', excel);
+// console.log('Excel: ', excel);
